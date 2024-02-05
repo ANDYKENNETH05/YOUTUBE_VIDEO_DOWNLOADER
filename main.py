57c67c93d599b02d5f3acf72b5692d22d23edb4c
Andy kenneth <andy.kenneth@binghamuni.edu.ng>
2024-02-05 11:42:14 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter
+from pytube import YouTube
+from tkinter import *
+
+app = Tk()
+app.title("YOUTUBE VIDEO DOWNLOADER")
+app.config(background="green")
+app.geometry("600x400")
+
+app.mainloop()
