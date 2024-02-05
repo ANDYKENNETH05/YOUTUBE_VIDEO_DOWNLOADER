41777c8d80696d38c714c9d8979b67c3d3be1b80
Andy kenneth <andy.kenneth@binghamuni.edu.ng>
2024-02-05 21:27:06 +0100

    designed the gui

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 import tkinter as tk
-from pytube import YouTube
 import customtkinter
+from pytube import YouTube
 from tkinter import filedialog
 
-def startDownload(option):
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("YOUTUBE DOWNLOADER")
+
+def start_download(option):
     try:
         ytLink = link.get()
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
@@ -20,7 +24,8 @@ def startDownload(option):
             video.download(file_path_lq)
         elif option == "audio":
             video = ytObject.streams.get_audio_only()
-            file_path_audio = filedialog.asksaveasfilename(defaultextension=".mp3", filetypes=[("Audio files", "*.mp3")])
+            file_path_audio = filedialog.asksaveasfilename(defaultextension=".mp3",
+                                                           filetypes=[("Audio files", "*.mp3")])
             video.download(file_path_audio)
         else:
             return
@@ -32,20 +37,40 @@ def startDownload(option):
     except:
         finishLabel.configure(text="Download Error", text_color="red")
 
-=======
->>>>>>> b8b6495fbd210222dd3b2adb7d83b5749e0d97a3
-import tkinter
-from pytube import YouTube
-from tkinter import *
 
-app = Tk()
-app.title("YOUTUBE VIDEO DOWNLOADER")
-app.config(background="green")
-app.geometry("600x400")
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_download = total_size - bytes_remaining
+    percentage_of_completion = bytes_download / total_size * 100
+    per = str(int(percentage_of_completion))
+    profile.configure(text=per + "%")
+    progress.update()
 
-<<<<<<< HEAD
+
+title = customtkinter.CTkLabel(app, text="INSERT YOUTUBE LINK", font=("times new roman", 22), )
+title.pack(padx=10, pady=10)
+
+url_var = tk.StringVar()
+link = customtkinter.CTkEntry(app, width=400, height=30, placeholder_text="paste video link here....",
+                              textvariable=url_var)
+link.pack()
+
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+progress = customtkinter.CTkLabel(app, text="0%")
+progress.pack()
+
+download = customtkinter.CTkButton(app, text="DOWNLOAD HIGH QUALITY-mp4", corner_radius=20, hover_color="green", border_width=2,
+                                   border_color="black", command=lambda: start_download("hq"))
+download.pack(padx=20, pady=10)
+
+download2 = customtkinter.CTkButton(app, text="DOWNLOAD low QUALITY-mp4", corner_radius=20, hover_color="green", border_width=2,
+                                    border_color="black", command=lambda: start_download("lq"))
+download2.pack(padx=20, pady=10)
+
+download3 = customtkinter.CTkButton(app, text="DOWNLOAD mp3", corner_radius=20, hover_color="green", border_width=2,
+                                    border_color="black", command=lambda: start_download("audio"))
+download3.pack(padx=20, pady=10)
+
 app.mainloop()
-=======
-app.mainloop()
->>>>>>> 81f6ac77b30f05d2a547dfae03dff86cc8497363
->>>>>>> b8b6495fbd210222dd3b2adb7d83b5749e0d97a3
